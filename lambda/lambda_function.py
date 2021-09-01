896e2b4fd2dcd1176203db0443fe890cd908fd80
Ubuntu <ubuntu@ip-172-31-88-212.ec2.internal>
2021-09-01 15:01:29 +0000

    modified lambda source

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = ec2.describe_instances()
     return {"statusCode": 200, "body": json.dumps(response)}
