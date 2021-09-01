2290b4de63ceb4b9b4d3487aef154c3390ea61e1
Ubuntu <ubuntu@ip-172-31-88-212.ec2.internal>
2021-09-01 16:01:09 +0000

    modified lambda script

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,7 +1,3 @@
-import boto3
-import json
+# This program prints Hello, world!
 
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_instances()
-    return {"statusCode": 200, "body": json.dumps(response)}
+print('Hello, world!')
